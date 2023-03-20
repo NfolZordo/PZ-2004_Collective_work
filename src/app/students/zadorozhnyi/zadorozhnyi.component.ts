76097823de37117c734a06ed41cf8d38e2a77afe
NfolZordo <79935610+NfolZordo@users.noreply.github.com>
2023-03-20 10:07:18 +0200

    add Zador page

--- src/app/students/zadorozhnyi/zadorozhnyi.component.ts
+++ src/app/students/zadorozhnyi/zadorozhnyi.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class ZadorozhnyiComponent {
 
+  playVideo() {
+    let video = document.getElementById('myVideo') as HTMLVideoElement;
+    video.play();
+  }
 }
