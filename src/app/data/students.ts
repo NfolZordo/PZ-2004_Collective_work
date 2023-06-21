06cda79eacea006fdb5ecbe1ec327db61e3ce0a9
tsybenko1523 <viecheslavtsybenko95@gmail.com>
2023-06-21 15:00:12 +0300

    Change photo format

--- src/app/data/students.ts
+++ src/app/data/students.ts
@@ -126,7 +126,7 @@ export const students: IStudent[] = [
         "firstName": "В'ячеслав",
         "lastName": "Цибенко",
         "middleName": "Олександрович",
-        "photo": "assets/18601.png",
+        "photo": "assets/tsybenko/photo1.png",
         "url": "/tsybenko",
         "audioFilePath": "assets/audio-students/student-theme.mp3"
     },
